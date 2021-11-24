0954d8a723d017417f495fefd4c8a565d78b6101
liuds <1015314611@qq.com>
2021-11-24 23:03:12 +0800

    更新

---
+++ p2p-exterface/src/main/java/com/bjpowernode/p2p/service/loan/BidInfoService.java
@@ -0,0 +1,7 @@
+﻿package com.bjpowernode.p2p.service.loan;
+
+public interface BidInfoService {
+
+    /**获取平台投资总金额*/
+    Double queryTotalBidMoney();
+}
---
+++ p2p-exterface/src/main/java/com/bjpowernode/p2p/service/user/UserService.java
@@ -0,0 +1,7 @@
+﻿package com.bjpowernode.p2p.service.user;
+
+public interface UserService {
+
+    /**获取平台注册总人数*/
+    Long queryAllUserCount();
+}
